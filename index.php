b675ff8b5d4433eff27a9e44d489114a61333781
LucasCarvalho23 <lucasilva1990@hotmail.com>
2024-09-20 16:08:28 -0300

    group route

--- index.php
+++ index.php
@@ -7,9 +7,18 @@
     $app->get('/postagens', function(){
         echo "Lista de mensagens";
     });
-    $app->get('/usuarios/{id}', function($request, $response){
+    $app->get('/usuarios[/{id}]', function($request, $response){
         $id = $request->getAttribute('id');
         echo "ID: ". $id;
+    });
+
+    $app->group('/v1', function() {
+        $this->get('/livros', function() {
+            echo "Lista de livros";
+        });
+        $this->get('/filmes', function() {
+            echo "Lista de filmes";
+        });
     });
 
     $app->run(); 

5f0a6ae87df011123787ba8eaf57f40e29562775
LucasCarvalho23 <lucasilva1990@hotmail.com>
2024-09-20 15:00:51 -0300

    default

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    require 'vendor/autoload.php';
+
+    $config = ['settings' => [
+    'addContentLengthHeader' => false,
+]];
+$app = new \Slim\App($config);
+
+// Define app routes
+$app->get('/hello/{name}', function ($request, $response, $args) {
+    return $response->write("Hello " . $args['name']);
+});
+
+// Run app
+$app->run();
+
+?>

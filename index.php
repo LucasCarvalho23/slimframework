c0439567b26ecb0bd6dbaff1932967d160ce2657
LucasCarvalho23 <lucasilva1990@hotmail.com>
2024-09-23 16:32:06 -0300

    database

--- index.php
+++ index.php
@@ -2,46 +2,43 @@
 
     use \Psr\Http\Message\ServerRequestInterface as Request;
     use \Psr\Http\Message\ResponseInterface as Response;
+    use Illuminate\Database\Capsule\Manager as Capsule;
 
     require 'vendor/autoload.php';
 
     $app = new \Slim\App;
 
-    $app->get('/header', function(Request $request, Response $response) {
-        $response->write('Esse é um retorno header');
-        return $response->withHeader('allow', 'PUT')
-                        ->withAddedHeader('Content-Length', '10');
-    });
+    $container = $app->getContainer();
+    $container['db'] = function() {
 
-    $app->get('/json', function(Request $request, Response $response) {
-        return $response->withJson([
-            "nome" => "Lucas Carvalho",
-            "nome2" => "Maria Eduarda"
+        $capsule = new Capsule;
+
+        $capsule->addConnection([
+            'driver' => 'mysql',
+            'host' => 'locahost',
+            'database' => 'database',
+            'username' => 'root',
+            'password' => 'password',
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => ''
         ]);
+
+        $capsule->setAsGlobal();
+        $capsule->bootEloquent();
+        return $capsule;
+    };
+
+    $app->get('/usuarios', function(Request $request, Response $response) {
+        $db = $this->get('db')->schema();
+        $db->dropIfExists('teste');
+        $db->create('teste', function($table) {
+            $table->increments('id');
+            $table->string('nome');
+            $table->string('email');
+            $table->timestamps();
+        });
     });
-
-    $app->get('/xml', function(Request $request, Response $response) {
-        $xml = file_get_contents('arquivo');
-        $response->write($xml);
-        return $response->withHeader('Content-Type', 'application/xml');
-    });
-
-    $app->add(function(Request $request, Response $response, $next) {
-        $response->write('This is Middleware + ');
-        $response = $next($request, $response);
-        $response->write(' + End the middleware.');
-        return $response;
-    });
-
-    $app->get('/usuario', function(Request $request, Response $response) {
-        $response->write('This is Usuário');
-    });
-
-    $app->get('/postagem', function(Request $request, Response $response) {
-        $response->write('This is Postagem');
-    });
-
-
 
     $app->run(); 
     
